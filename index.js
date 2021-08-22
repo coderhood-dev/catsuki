cbb87303e2b39cff7376d2f83b14efb7f5deb590
Ema Lorenzo <aemalorenzo@gmail.com>
2021-08-22 20:26:04 -0300

    event listeners

--- index.js
+++ index.js
@@ -8,17 +8,52 @@ button.addEventListener('click', () => {
   }
 })
 
-const cards = document.querySelectorAll('.card')
+const renderCatCard = (cardIndex) => {
+  const card = document.createElement('div')
+  card.classList.add('card')
 
-for (let i = 0; i < cards.length; i++) {
-  const card = cards[i]
+  const image = document.createElement('img')
+  image.src = 'https://cataas.com/cat'
+  image.alt = 'Catsuki'
 
+  card.appendChild(image)
+
+  // agrego el indicador de indice en la card
   const indexElement = document.createElement('p')
-  indexElement.innerHTML = i + 1
+  indexElement.innerHTML = cardIndex + 1
 
   card.appendChild(indexElement)
 
+  // agrego la funcionalidad de eliminar la card al hacerle click
   card.addEventListener('click', () => {
     card.remove()
   })
+
+  cardsContainer.appendChild(card)
 }
+
+const cardsContainer = document.getElementsByClassName('catalogue-content')[0]
+const addButton = document.getElementById('add-cat-button')
+
+addButton.addEventListener('click', renderCatCard)
+
+// empieza a cargar
+// muestra un indicador de que esta cargando
+// una vez que carga muestra los elementos
+
+const getCatCards = () => {
+  // traer las cards de gatos de la base de datos
+  setTimeout(() => {
+    const loaderIndicator = document.getElementById('catalogue-loader')
+    loaderIndicator.style.display = 'none'
+
+    const catCards = [1, 2, 3, 4, 5]
+  
+    for (let i = 0; i < catCards.length; i++) {
+      renderCatCard(i)
+    }
+  }, 3000)
+}
+
+// cargar las cards de gatos y mostrarlas en pantalla
+getCatCards()
